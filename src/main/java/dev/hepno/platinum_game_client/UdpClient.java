6a3fbd6fc83611848738932d556bc5a9e76a28db
Lily Lindstrand <lily@hepno.dev>
2025-02-22 21:53:31 +0100

    feat(packet): make the udp client send packet objects

--- src/main/java/dev/hepno/platinum_game_client/UdpClient.java
+++ src/main/java/dev/hepno/platinum_game_client/UdpClient.java
@@ -1,6 +1,9 @@
 package dev.hepno.platinum_game_client;
 
+import dev.hepno.platinum_api.packet.Packet;
+import dev.hepno.platinum_api.packet.PacketType;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
@@ -11,9 +14,12 @@ import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.util.CharsetUtil;
 import io.netty.util.internal.SocketUtils;
 
+import java.io.ByteArrayOutputStream;
+import java.net.InetSocketAddress;
+
 public class UdpClient {
 
-    static final int PORT = 7777; 
+    static final int PORT = 7777;
     private EventLoopGroup eventLoopGroup;
     private Bootstrap bootstrap;
     private Channel channel;
@@ -28,20 +34,24 @@ public class UdpClient {
                     .option(ChannelOption.SO_BROADCAST, true)
                     .handler(new UdpClientHandler());
             channel = bootstrap.bind(0).sync().channel();
-
-            // Broadcast the request to port 8080
-            channel.writeAndFlush(new DatagramPacket(
-                    Unpooled.copiedBuffer("QOTM?", CharsetUtil.UTF_8),
-                    SocketUtils.socketAddress("127.0.0.1", PORT))).sync();
-
-            // Timeout
-            if (!channel.closeFuture().await(5000)) {
-                System.err.println("Request timed out.");
-            }
         } finally {
             eventLoopGroup.shutdownGracefully();
         }
 
+    }
+
+    public void write(Packet packet) throws InterruptedException {
+        ByteBuf byteBuf = Unpooled.buffer();
+        PacketType packetType = PacketType.getPacketType(packet);
+        byteBuf.writeInt(packetType.ordinal());
+        packet.encode(byteBuf);
+
+        DatagramPacket datagramPacket = new DatagramPacket(
+                byteBuf,
+                new InetSocketAddress("127.0.0.1", PORT)
+        );
+
+        channel.writeAndFlush(datagramPacket).sync();
     }
 
 }
